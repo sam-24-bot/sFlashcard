28811fce7981f26a1af2fee54530070fa63b7a2e
Sam <sam@Samanthas-MacBook-Air.local>
2021-02-21 19:56:25 -0800

    second commit

--- app/src/main/java/com/example/sflashcard/MainActivity.java
+++ app/src/main/java/com/example/sflashcard/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sflashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView FlashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView FlashcardAnswer = findViewById(R.id.flashcard_answer);
+
+
+        FlashcardQuestion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FlashcardQuestion.setVisibility(View.INVISIBLE);
+                FlashcardAnswer.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 }
