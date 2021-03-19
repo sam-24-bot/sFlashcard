099462ed1ce1bbb6400730ebd2e4a3195226b150
Sam <sam@Samanthas-MacBook-Air.local>
2021-03-19 16:07:04 -0700

    fifth commit

--- app/src/main/java/com/example/sflashcard/AddCardActivity.java
+++ app/src/main/java/com/example/sflashcard/AddCardActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
     @Override
@@ -27,12 +28,19 @@ public class AddCardActivity extends AppCompatActivity {
                 String card_question = ((EditText) findViewById(R.id.user_question)).getText().toString();
                 String card_answer = ((EditText) findViewById(R.id.user_answer)).getText().toString();
 
+                if (card_question.equals("") || card_answer.equals("")){
+                    Toast.makeText(getApplicationContext(), "Add text", Toast.LENGTH_SHORT).show();
 
-                Intent data = new Intent(); // create a new Intent, this is where we will put our data
-                data.putExtra("question", card_question); // puts one string into the Intent, with the key as 'string1'
-                data.putExtra("answer", card_answer); // puts another string into the Intent, with the key as 'string2
-                setResult(RESULT_OK, data); // set result code and bundle data for response
-                finish(); // closes this activity and pass data to the original activity that launched this activity
+                }
+                else{
+                    Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                    data.putExtra("question", card_question); // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("answer", card_answer); // puts another string into the Intent, with the key as 'string2
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish(); // closes this activity and pass data to the original activity that launched this activity
+
+                }
+
             }
 
         });
--- app/src/main/java/com/example/sflashcard/MainActivity.java
+++ app/src/main/java/com/example/sflashcard/MainActivity.java
@@ -6,13 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
+import java.util.*;
+import com.google.android.material
+        .snackbar
+        .Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard>allFlashcards;
+    int currentCardDisplayedIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
         TextView FlashcardQuestion = findViewById(R.id.flashcard_question);
         TextView FlashcardAnswer = findViewById(R.id.flashcard_answer);
@@ -52,8 +69,34 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("answer", " ");
         MainActivity.this.startActivityForResult(intent, 100);
 
-    }
+        findViewById(R.id.next_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // don't try to go to next card if you have no cards to begin with
+                if (allFlashcards.size() == 0)
+                    return;
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
 
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(FlashcardQuestion,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getAnswer());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getQuestion());
+            }
+        });
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -64,6 +107,10 @@ public class MainActivity extends AppCompatActivity {
 
             ((TextView) findViewById(R.id.flashcard_question)).setText(question);
             ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
+
     }
 }
